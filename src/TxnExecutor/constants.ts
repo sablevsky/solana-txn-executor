4b9911f9157c005318f84c81bc08b9901fa45ff5
sablevsky <vladislav1145@gmail.com>
2024-04-22 11:17:34 +0200

    Implement new getPriorityFee function for TxnExecutor. Now getPriorityFee gets CreateTxnParam and called on each make txn

--- src/TxnExecutor/constants.ts
+++ src/TxnExecutor/constants.ts
@@ -1,6 +1,8 @@
+import { GetPriorityFee } from './types'
+
 //? To define that user has rejected txn (check error.message). 0 -- phantom error message, 1 -- solflare error message
 export const USER_REJECTED_TXN_ERR_MESSAGES = ['User rejected the request.', 'Transaction rejected']
 
 export const DEFAULT_CONFIRMATION_TIMEOUT = 60
 
-export const GET_PRIORITY_FEE_PLACEHOLDER = () => Promise.resolve(0)
+export const GET_PRIORITY_FEE_PLACEHOLDER: GetPriorityFee = () => Promise.resolve(0)
